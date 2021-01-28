2665acd5409d43a85e34e725e3d9f05aa48b9a99
[Mid00Night] <[ibrahemaboelhamedali@yahoo.com]>
2021-01-28 23:07:59 +0200

    Dart_Project

---
+++ main.dart
@@ -0,0 +1,192 @@
+import 'dart:io';
+
+int id;
+String location;
+int passengerlimit;
+String date;
+double price;
+main(List<String> args) {
+  int input;
+  while (true) {
+    print('If you want to:');
+    print('\n');
+    stdout.write('Add trip press 1 ' + '           ');
+    stdout.write('Edit trip press 2');
+    print('\n');
+    stdout.write('Delete trip press 3 ' + '        ');
+    stdout.write('Search for a trip by price press 4');
+    print('\n');
+    stdout.write('View trips press 5 ' + '         ');
+    stdout.write('Reserve a trip press 6 ' + '     ');
+    print('\n');
+    stdout.write('view passengers press 7' + '     ');
+    stdout.write('Exit press 8');
+    print('\n');
+    input = int.parse(stdin.readLineSync());
+    TravelAgency ta = TravelAgency();
+    if (input == 1) {
+      print('Enter ID : ');
+      id = int.parse(stdin.readLineSync());
+      print('Enter Location : ');
+      location = stdin.readLineSync();
+      print('Enter Passenger Limit : ');
+      passengerlimit = int.parse(stdin.readLineSync());
+      print('Enter Date : ');
+      date = stdin.readLineSync();
+      print('Enter Price \$ : ');
+      price = double.parse(stdin.readLineSync());
+
+      ta.AddTrip(id, location, passengerlimit, price, date);
+    } else if (input == 2) {
+      print('If you want to edit : ' +
+          '\n' +
+          'ID press 1' +
+          '\n' +
+          'Location press 2' +
+          '\n' +
+          'Passenger limit press 3' +
+          '\n' +
+          'Date press 4' +
+          '\n' +
+          'Price press 5');
+      int input1 = int.parse(stdin.readLineSync());
+      if (input1 == 1) {
+        print('Enter the new ID');
+        int input1 = int.parse(stdin.readLineSync());
+        ta.EditTrip(id, location, passengerlimit, price, date);
+      } else if (input1 == 2) {
+        print('Enter the new Location');
+        String location = stdin.readLineSync();
+        ta.EditTrip(id, location, passengerlimit, price, date);
+      } else if (input1 == 3) {
+        print('Enter the new Passenger Limit');
+        int passengerlimit = int.parse(stdin.readLineSync());
+        ta.EditTrip(id, location, passengerlimit, price, date);
+      } else if (input1 == 4) {
+        print('Enter the new Date');
+        String date = stdin.readLineSync();
+        ta.EditTrip(id, location, passengerlimit, price, date);
+      } else {
+        print('Enter the new Price');
+        double price = double.parse(stdin.readLineSync());
+        ta.EditTrip(id, location, passengerlimit, price, date);
+      }
+    } else if (input == 3) {
+      print('Enter ID');
+      int id1 = int.parse(stdin.readLineSync());
+      ta.DeleteTrip(id1);
+    } else if (input == 4) {
+      print('Enter the price');
+      double p = double.parse(stdin.readLineSync());
+    } else if (input == 5) {
+      ta.ViewTrips();
+    } else if (input == 6) {
+    } else if (input == 7) {
+    } else if (input == 8) {
+      exit(0);
+    }
+    print('Do you want to continue ?  y/n');
+    String c = stdin.readLineSync();
+    if(c == 'n')
+      {
+        exit(0);
+      }
+    else{
+      continue;
+    }
+  }
+}
+
+class TravelAgency {
+  int ID;
+  String Location;
+  int PassengerLimit;
+  String Date;
+  double Price;
+  List<Trips> myList = List<Trips>();
+  Trips t1 = Trips();
+
+  AddTrip(
+      int ID, String Location, int PassengerLimit, double Price, String Date) {
+    t1.ID = ID;
+    t1.Location = Location;
+    t1.PassengerLimit = PassengerLimit;
+    t1.Price = Price;
+    t1.Date = Date;
+
+    myList.add(t1);
+  }
+
+  EditTrip(
+      int ID, String Location, int PassengerLimit, double Price, String Date) {
+    t1.ID = ID;
+    t1.Location = Location;
+    t1.PassengerLimit = PassengerLimit;
+    t1.Price = Price;
+    t1.Date = Date;
+    myList.add(t1);
+  }
+
+  DeleteTrip(int ID) {
+    for (int i = 0; i <= myList.length; i++) {
+      if (ID == myList[i].ID) {
+        myList[i] == 0;
+      }
+    }
+    print('There are no trips with that ID');
+  }
+
+  ViewTrips() {
+    print('ID' +
+        '       ' +
+        'Location' +
+        '       ' +
+        'PassengerLimit' +
+        '       ' +
+        'Price' +
+        '       ' +
+        'Date');
+    for (int i = 0; i <= myList.length; i++) {
+      print('$myList[i].ID' +
+          '    ' +
+          '$myList[i].Location' +
+          '    ' +
+          '$myList[i].PassengerLimit' +
+          '    ' +
+          '$myList[i].Price' +
+          '    ' +
+          '$myList[i].Date');
+    }
+  }
+
+  SearchTrip(double Price) {
+    for (int i = 0; i <= myList.length; i++) {
+      if (Price == myList[i].Price) {
+        print(myList[i].toString());
+      }
+    }
+  }
+
+  Reverse(int ID) {}
+}
+
+class Trips {
+  int ID;
+  String Location;
+  int PassengerLimit;
+  String Date;
+  double Price;
+  List<Passengers> p = List<Passengers>();
+}
+class Passengers {
+  String name;
+  int PhoneNumber;
+  String Address;
+
+  setPassenger(String name , int PhoneNumber ,String Address )
+  {
+    this.name = name;
+    this.PhoneNumber = PhoneNumber;
+    this.Address = Address;
+  }
+}
